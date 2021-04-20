2e739c8cb99f28440584a0e2c97b499a5356970c
Bartłomiej Klocek <barthap10@gmail.com>
2021-04-20 15:07:02 +0200

    [client] add lint

--- client/server/server.js
+++ client/server/server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const path = require("path");
-const httpProxy = require("http-proxy");
+const express = require('express');
+const httpProxy = require('http-proxy');
+const path = require('path');
 
 // Constants
 const PORT = process.env.PORT || 4123;
-const HOST = "0.0.0.0";
+const HOST = '0.0.0.0';
 
-const API_URL = process.env.API_URL || "http://localhost:5000";
+const API_URL = process.env.API_URL || 'http://localhost:5000';
 
-const USERNAME = process.env.PANEL_USER || "demo";
-const PASSWORD = process.env.PANEL_PASS || "demo";
+const USERNAME = process.env.PANEL_USER || 'demo';
+const PASSWORD = process.env.PANEL_PASS || 'demo';
 
-const CLIENT_BUILD_PATH = path.join(__dirname, "../build");
+const CLIENT_BUILD_PATH = path.join(__dirname, '../build');
 
 // App
 const app = express();
@@ -21,27 +21,27 @@ const apiProxy = httpProxy.createProxyServer();
 app.use(express.static(CLIENT_BUILD_PATH));
 
 // for parsing application/json only in auth (do not use it in api proxy!)
-app.use("/auth", express.json());
+app.use('/auth', express.json());
 
 //simple credentials check (for frontend panel)
-app.post("/auth", function (req, res) {
+app.post('/auth', function (req, res) {
   const user = req.body.username;
   const pass = req.body.password;
 
   //just compare
-  if (user === USERNAME && pass === PASSWORD) res.status(200).send("OK");
+  if (user === USERNAME && pass === PASSWORD) res.status(200).send('OK');
   else res.sendStatus(401);
 });
 
 //redirect all API calls to API server (Spring Boot)
-app.all("/api/*", function (req, res) {
-  req.url = req.url.replace("/api", "");
+app.all('/api/*', function (req, res) {
+  req.url = req.url.replace('/api', '');
   apiProxy.web(req, res, { target: API_URL });
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get("*", function (_request, response) {
-  response.sendFile(path.join(CLIENT_BUILD_PATH, "index.html"));
+app.get('*', function (_request, response) {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
 app.listen(PORT, HOST);
--- client/src/components/ApiComponent.tsx
+++ client/src/components/ApiComponent.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-const API_URL = process.env.REACT_APP_API_URL || "/api";
+const API_URL = process.env.REACT_APP_API_URL || '/api';
 
 export default function ApiComponent() {
-  const [status, setStatus] = useState("loading...");
+  const [status, setStatus] = useState('loading...');
 
   const fetchServer = async () => {
     try {
-      const result = await fetch(API_URL + "/");
+      const result = await fetch(API_URL + '/');
       return result.text();
     } catch (e) {
       console.error(e);
-      return "Down :(";
+      return 'Down :(';
     }
   };
 
   useEffect(() => {
-    fetchServer().then((body) => setStatus(body));
+    fetchServer().then(body => setStatus(body));
   }, []);
 
   return <p>Server status is: {status}</p>;
