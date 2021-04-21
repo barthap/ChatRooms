de9aa5dc9255f7e02025b9e860e2dcf32c8e2f26
Bartłomiej Klocek <barthap10@gmail.com>
2021-04-21 15:20:30 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,14 @@
 
 set -euo pipefail
 
+echo "Setting up environment..."
+
 export VIRTUAL_HOST=$APP_DOMAIN
 export LETSENCRYPT_HOST=$APP_DOMAIN
 export REGISTRY_NAME=$REGISTRY_NAME
 
 SERVER_URL=https://$APP_DOMAIN
 
-# set host url from env
-# it is used just to avoid storing all information
-# outside this GH repo (it could be stored on deployment host directly)
 echo "REACT_APP_SERVER_URL=$SERVER_URL" > client.env
 
 echo "Pulling latest images..."
