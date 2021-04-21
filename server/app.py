6b02016cd9e9f685e1e16c6104e735b99a6a8bd8
Bartłomiej Klocek <barthap10@gmail.com>
2021-04-21 11:10:15 +0200

    Add example server responses

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 import logging
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit, send
 
 
 app = Flask(__name__)                           # Create Flask instance
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = 'ChatRoomsSecret'    # Change me
 cors = CORS(app)                                # Enable global CORS for HTTP requests
 
 # Create the Socket.io instance, also enable CORS
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins='*', logger=True)
 
 @app.route('/')
 def hello_world():
@@ -17,20 +17,32 @@ def hello_world():
 
 @app.route('/health')
 def health():
+    """
+    API Endpoint to check if server is responding correctly.
+    Client can send `GET /api/health` to check if server is ready.
+    """
     return 'OK'
 
+# listen to global event named "message"
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
 
+# listen to events named json
 @socketio.on('json')
 def handle_json(json):
     print('received json: ' + str(json))
 
+# listen to events named "my event"
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received my event: ' + str(json))
 
+    # this sends information to global "message" event
+    send('received my event: ' + str(json))
+
+    # this emits event with name "responses"
+    emit('responses', json)
+
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
-
